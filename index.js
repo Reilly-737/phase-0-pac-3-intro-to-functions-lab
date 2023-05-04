e63234d28335979eba18e0b2d3771a63078074cc
+ <+>
2023-05-04 13:49:45 -0600

    finished test index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+function shout(string){
+    "Hello!".toUpperCase(); // 'HELLO!'
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    "HELLO!".toLowerCase(); // 'hello!'
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase())
+
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+
+function whisper(string){
+    'I can/t hear you!'.toLowerCase(); // 'i can't hear you!'
+    return string .toLowerCase();
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+ function sayHiToHeadphonedRoommate(string){
+     if (string === string.toLowerCase()){
+        return "I can't hear you!"
+     } 
+     if (string === string.toUpperCase()){
+        return "YES INDEED!"
+     } 
+     if (string === "Let's have dinner together!"){
+         return "I would love to!"
+     }
+     
+   
+   
+    // "I can't hear you!".toLowerCase(); // 'i can't hear you'
+    // return string.toLowerCase();
+ }
+
+
+
+
+  
+    
+
+
+
+
+
+
+
+
+
+
+
